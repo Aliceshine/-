1719a9dde1fded0247a77976893564053507570f
苇草-May <lyttgscxl@163.com>
2019-04-24 10:42:47 +0800

    first init

---
+++ youfanshopuser/src/main/java/com/youfan/model/User.java
@@ -0,0 +1,42 @@
+package com.youfan.model;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
